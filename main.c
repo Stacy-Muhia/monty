ec3b4ab7606f07563c49ee704a457a8a81c308fb
MwendeHarriet <mwendeharriet@gmail.com>
2023-08-22 23:32:27 -0700

    main function prototype

---
+++ main.c
@@ -0,0 +1,25 @@
+#include "monty.h"
+
+/**
+ * main-entry point
+ *
+ *
+ */
+int main(int argc, char *argv[])
+{#include "monty.h"
+
+	/**
+	 * main-entry point
+	 *
+	 *
+	 */
+	int main(int argc, char *argv[])
+	{#include "monty.h"
+
+		/**
+		 * main-entry point
+		 *
+		 *
+		 */
+		int main(int argc, char *argv[])
+		{
